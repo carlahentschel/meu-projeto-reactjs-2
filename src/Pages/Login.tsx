5993dedf7264921255523e746707b87abfcc2a4f
Jessica de Lurdes Fischer <jessicadelfischer@gmail.com>
2023-03-09 17:56:23 -0300

    Alterada imagem inserido botão.
    Co-authored-by: Carla Regina Hentschel <carlahentschel@users.noreply.github.com>
    Co-authored-by: Paola Albarnaz <palbarnaz@users.noreply.github.com>
    Co-authored-by: maricotamaidana@hotmail.com <maricotamaidana@hotmail.com>
    Co-authored-by: Dionei Silva <dionei-silva@users.noreply.github.com>

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,31 +1,32 @@
-import { Checkbox, Grid, TextField, Box } from "@mui/material"
-
 import React from "react"
+import { Checkbox, Grid, TextField, Box, FormGroup, FormControlLabel, Button } from "@mui/material"
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
-const Login: React.FC = ()=>{
+const Login: React.FC = ()=> {
   return (
     <>
-        <Grid container height={'100vh'} 
-  justifyContent="center"
-  alignItems="center">
-            <Grid item sm={7} height={"100%"}>
-          <img src="https://source.unsplash.com/random"/>
+        <Grid container height={'100vh'} width={'100vw'} justifyContent="center" alignItems="center">
+            <Grid item sm={7} height={"100%"} width={"60%"}>
+          <img src= "./assets/images/image.jpg" />
             </Grid>
 
-            <Grid item sm={5}></Grid>
-              <Grid container 
+            <Grid item sm={5}>
+            <Grid container 
               flexDirection={'column'}
               height={'100vh'}
               >
               <Box component={'form'} >
                     <TextField id="email-adress" label="Email Adress" variant="outlined" />
                     <TextField id="password" label="Password" variant="outlined" />
-                    <Checkbox {...label} defaultChecked />
-
+                    <FormGroup>
+                      <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me" />
+                    </FormGroup>
+                    <Button variant="contained">SING IN</Button>
                 </Box>
               </Grid>
+            </Grid>
+              
             </Grid>
 
 
